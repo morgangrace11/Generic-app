c2b73c849a676696480e3672e0823c59a268efae
morgangrace11 <morgangrace720@gmail.com>
2018-12-04 00:30:14 -0500

    did generic app setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//The purpose of this file is o serve as the entry point of our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
